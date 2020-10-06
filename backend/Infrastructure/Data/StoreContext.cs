2481d560173ab3f33443eb4675ed5294f30384b3
UxDeveloper82 <electronicsmf@aol.com>
2020-10-06 07:14:40 -0400

    API Architecture

--- backend/Infrastructure/Data/StoreContext.cs
+++ backend/Infrastructure/Data/StoreContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,14 @@ namespace Infrastructure.Data
         public StoreContext(DbContextOptions<StoreContext> options) : base(options){}
 
         public DbSet<Product> Products { get;  set; }
+        
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
